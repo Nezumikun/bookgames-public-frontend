5a7fcdbdfd75a2901ad28fd15fc529f9981802cd
Alexey Kozhevnikov <alexey.kozhevnikov@gmail.com>
2020-07-26 15:12:54 +0300

    Редизайн списка игра на карточки. В шапку добавлено московское время

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template lang="pug">
   div(id="app")
     b-navbar(variant="dark" fixed="top" sticky=true type="dark")
-      b-navbar-brand(to="/")
+      b-navbar-brand(to="/" class="mr-auto")
         b-icon-book
         span(class="pl-2") Книгоигры
+      b-navbar-nav
+        b-nav-text(class="text-right") Московское время: {{ moscowTime.setZone("Europe/Moscow").toFormat('dd.MM.yyyy HH:mm') }}
     router-view
 </template>
 
 <script>
 import { BIcon, BIconBook } from 'bootstrap-vue'
+import { DateTime } from 'luxon'
 
 export default {
+  data () {
+    return {
+      moscowTime: DateTime.local()
+    }
+  },
+  created () {
+    setInterval(function () {
+      this.moscowTime = DateTime.local()
+    }.bind(this), 1000)
+  },
   components: {
     BIcon,
     BIconBook
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -33,26 +33,35 @@
           :key="item.id"
         )
           b-card(
-            :to="'/game/'+item.id"
             class="my-3"
           )
-            b-card-body
+            b-card-body(class="m-0 p-0")
               b-card-title {{ item.title }}
+              b-card-sub-title {{ item.subtitle }}
 
-              b-button-group
-                b-button(:to="'/game/'+item.id+'/logs'" variant="primary")
-                  b-icon-receipt
-                  span(class="pl-1") Ход игры
-                b-button(:to="'/game/'+item.id+'/result'" variant="success")
-                  b-icon-table
-                  span(class="pl-1") Таблица
+              b-card-text(class="mt-2")
+                div Начата: {{ formatTime(item.activatedTime) }}
+                div Закончена: {{ formatTime(item.finishTime) }}
+                div Вопросов: {{ item.countQuestion }}
+                div Участников: {{ item.countTeam }}
+
+              b-button(:to="'/game/'+item.id" variant="link" class="pl-0 pr-4")
+                b-icon-question-square
+                span(class="pl-1") Вопросы
+              b-button(:to="'/game/'+item.id+'/logs'" variant="link" class="pl-0 pr-4")
+                b-icon-receipt
+                span(class="pl-1") Ход игры
+              b-button(:to="'/game/'+item.id+'/result'" variant="link" class="pl-0 pr-4")
+                b-icon-table
+                span(class="pl-1") Результаты
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import apiCall from '../tools/axios'
-import { BIcon, BIconTable, BIconReceipt } from 'bootstrap-vue'
+import { BIcon, BIconTable, BIconReceipt, BIconQuestionSquare } from 'bootstrap-vue'
+import { DateTime } from 'luxon'
 
 export default {
   data () {
@@ -69,6 +78,11 @@ export default {
       return this.games.filter(x => x.isComplited === 'Y')
     }
   },
+  methods: {
+    formatTime (isoString) {
+      return DateTime.fromISO(isoString).setZone('Europe/Moscow').setLocale('ru').toFormat('dd MMMM yyyy в HH:mm')
+    }
+  },
   async created () {
     let res = await apiCall('games', 'get')
     this.games = res.data
@@ -78,7 +92,8 @@ export default {
   components: {
     BIcon,
     BIconTable,
-    BIconReceipt
+    BIconReceipt,
+    BIconQuestionSquare
   }
 }
 </script>
